b9cb9fe6158e8f0ee34363e521c1bb50db7ba0cc
parkgayoung96 <lunatic2563@gmail.com>
2023-01-10 19:08:17 +0900

    2023/01/10 수정

--- User/src/main/java/kr/co/user/dao/User1DAO.java
+++ User/src/main/java/kr/co/user/dao/User1DAO.java
@@ -3,32 +3,17 @@ package kr.co.user.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import kr.co.user.vo.User1VO;
 
 @Mapper
 @Repository
-public class User1DAO {
-
-	@Autowired
-	private SqlSessionTemplate mybatis;
+public interface User1DAO {
 	
-	public void insertUser1(User1VO vo) {
-		mybatis.insert("insertUser1", vo);
-	}
-	public User1VO selectUser1(String uid) {
-		return mybatis.selectOne("selectUser1", uid);
-	}
-	public List<User1VO> selectUser1s() {
-		return mybatis.selectList("selectUser1s");
-	}
-	public void updateUser1(User1VO vo) {
-		mybatis.update("udpateUser1", vo);
-	}
-	public void deleteUser1(String uid) {
-		mybatis.delete("deleteUser1", uid);
-	}
+	public void insertUser1(User1VO vo);
+	public User1VO selectUser1(String uid);
+	public List<User1VO> selectUser1s();
+	public void updateUser1(User1VO vo);
+	public void deleteUser1(String uid);
 }
